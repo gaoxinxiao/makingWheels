bf6d4b3f928e99ef76060096896ddb33f3185db4
gaoxinxiao <18210833386@163.com>
2021-03-26 20:36:12 +0800

    promise更新

---
+++ promise.js
@@ -0,0 +1,62 @@
+// new Promise((resolve, reject) => {
+// setTimeout(() => {
+//     resolve(1)
+// }, 1000)
+//     resolve(1)
+
+
+// }).then(v => {
+//     console.log('res1', v)
+// })
+
+function IPromise(fn) {
+    let state = 'padding'
+    let value = null
+    let callbcaks = []
+    this.then = function (onFuilled) {
+        return new IPromise((resolve) => {
+            handel({
+                resolve,
+                onFuilled
+            })
+        })
+    }
+    function handel(callback) {
+        if (state === 'padding') {
+            callbcaks.push(callback)
+            return
+        }
+        if (state === 'fulfilled'){
+            if (!callback.onFuilled){
+                callback.resolve(value)
+                return
+            }
+            let ret = callback.onFuilled(value)
+            callback.resolve(ret)
+        }
+    }
+    function handleCb() {
+        while (callbcaks.length) {
+            const fulfilledFn = callbcaks.shift()
+            handel(fulfilledFn)
+        }
+    }
+    function resolve(newVal) {
+        const fn = () => {
+            if (state !== 'padding') return
+            state = 'fulfilled'
+            value = newVal
+            handleCb()
+        }
+        setTimeout(fn, 0)
+    }
+    fn(resolve)
+}
+new IPromise((resolve, reject) => {
+    // setTimeout(() => {
+    //     resolve(1)
+    // }, 1000)
+    resolve('令狐冲')
+}).then(v => {
+    console.log('res1', v)
+})
