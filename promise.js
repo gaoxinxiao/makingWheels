aec3e7f2c01c71cc663014988afea0379f77cdf6
gaoxinxiao <18210833386@163.com>
2021-03-26 20:48:12 +0800

    1

--- promise.js
+++ promise.js
@@ -3,8 +3,6 @@
 //     resolve(1)
 // }, 1000)
 //     resolve(1)
-
-
 // }).then(v => {
 //     console.log('res1', v)
 // })
@@ -28,10 +26,11 @@ function IPromise(fn) {
         }
         if (state === 'fulfilled'){
             if (!callback.onFuilled){
+                //then没有回调函数的时候继续向下抛数据
                 callback.resolve(value)
                 return
             }
-            let ret = callback.onFuilled(value)
+            let ret = callback.onFuilled(value) //获取到结果继续向下抛
             callback.resolve(ret)
         }
     }
@@ -51,12 +50,16 @@ function IPromise(fn) {
         setTimeout(fn, 0)
     }
     fn(resolve)
+
 }
 new IPromise((resolve, reject) => {
     // setTimeout(() => {
     //     resolve(1)
     // }, 1000)
     resolve('令狐冲')
+}).then(v => {
+    console.log('res1', v)
+    return '风清扬'
 }).then(v => {
     console.log('res1', v)
 })
